b1ead8fc4547de7772a3786be8b4dfcfa5ae481a
Pavlo Goncharenko <pashaghnp@gmail.com>
2022-01-01 19:29:59 +0100

    Make viber messsage Id long

--- DecisionTelecom/Models/ViberMessageReceipt.cs
+++ DecisionTelecom/Models/ViberMessageReceipt.cs
@@ -8,7 +8,7 @@ namespace DecisionTelecom.Models
         /// Id of the Viber message which status should be got (sent in the last 5 days)
         /// </summary>
         [JsonPropertyName("message_id")]
-        public int ViberMessageId { get; set; }
+        public long ViberMessageId { get; set; }
 
         /// <summary>
         /// Viber message status
